32cec991759c8089ca3a30f22a1d564cdf6e311f
Krishna Teja <packetcode@gmail.com>
2020-04-09 07:19:09 +0530

    few changes

--- day1/audio_start.py
+++ day1/audio_start.py
@@ -1,8 +1,10 @@
 # # Creating an audio file
 
 # import google text to speech library
+from gtts import gTTS
 
 # process text to audio
-
+text = gTTS("This is a message from Krishna Teja")
 
 # write the file 
+text.save('audio2.mp3')
--- day1/varaibles_start.py
+++ day1/varaibles_start.py
@@ -1,10 +1,11 @@
 # # Variables in Python
 
 # Variable declaration
-
+a = 10
 
 # Changing variable values
-
-
+a = 20
+print(a)
 
 # Concatination of different type of varaibles
+print("Im a string" + str(123))
